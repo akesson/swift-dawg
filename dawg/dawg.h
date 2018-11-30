de046e7ce86525dcd6c5b3ce9013345d336ef580
Henrik <henrik@akesson.mobi>
2018-11-30 18:17:51 +0100

    Skeleton Xcode project

---
+++ dawg/dawg.h
@@ -0,0 +1,19 @@
+//
+//  dawg.h
+//  dawg
+//
+//  Created by Henrik Akesson on 30/11/2018.
+//  Copyright © 2018 Henrik Akesson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for dawg.
+FOUNDATION_EXPORT double dawgVersionNumber;
+
+//! Project version string for dawg.
+FOUNDATION_EXPORT const unsigned char dawgVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dawg/PublicHeader.h>
+
+
