c877a0b6bc0118a2c120c07193e733a3fcdc0904
Henrik <henrik@akesson.mobi>
2018-11-30 22:45:56 +0100

    Unified to one header file

---
+++ source/Dawg.h
@@ -0,0 +1,19 @@
+//
+//  Dawg.h
+//  Dawg
+//
+//  Created by Henrik Akesson on 30/11/2018.
+//  Copyright © 2018 Henrik Akesson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Dawg.
+FOUNDATION_EXPORT double Dawg_VersionNumber;
+
+//! Project version string for Dawg.
+FOUNDATION_EXPORT const unsigned char Dawg_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dawg_macOS/PublicHeader.h>
+
+
